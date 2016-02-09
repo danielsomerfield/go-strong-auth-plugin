b3516a2c4f41ee008f4aaba9481929256ad7056a
Daniel Somerfield <daniel@redwinewithfish.org>
2016-02-08 21:47:46 -0800

    Basic plugin structure in place.

--- src/main/java/com/thoughtworks/go/strongauth/JSONUtils.java
+++ src/main/java/com/thoughtworks/go/strongauth/JSONUtils.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.go.strongauth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
+import com.google.gson.GsonBuilder;
 
 public class JSONUtils {
-//    public static Object fromJSON(String json) {
-//        return new GsonBuilder().create().fromJson(json, Object.class);
-//    }
-
-    @SneakyThrows //TODO: Option
-    public static String toJSON(Object object) {
-        return new ObjectMapper().writeValueAsString(object);
+    public static Object fromJSON(String json) {
+        return new GsonBuilder().create().fromJson(json, Object.class);
     }
+
+    public static String toJSON(Object object) {
+        return new GsonBuilder().create().toJson(object);
+    }
+
 }
--- src/main/java/com/thoughtworks/go/strongauth/StrongAuthPlugin.java
+++ src/main/java/com/thoughtworks/go/strongauth/StrongAuthPlugin.java
@@ -1,64 +1,96 @@
 package com.thoughtworks.go.strongauth;
 
-import com.thoughtworks.go.plugin.api.AbstractGoPlugin;
+import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
+import com.thoughtworks.go.plugin.api.GoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
-import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.logging.Logger;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
-import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.singletonList;
+import static java.util.Arrays.asList;
 
 @Extension
-public class StrongAuthPlugin extends AbstractGoPlugin {
-
-    private static final List<String> SUPPORTED_VERSIONS = singletonList("1.0");
-
-    public static final String EXTENSION_NAME = "strongauth";
-    public static final String PLUGIN_SETTINGS_GET_CONFIGURATION = "go.plugin-settings.get-configuration";
+public class StrongAuthPlugin implements GoPlugin {
+    public static final String EXTENSION_NAME = "authentication";
+    public static final String PLUGIN_CONFIGURATION = "go.authentication.plugin-configuration";
+    public static final int SUCCESS_RESPONSE_CODE = 200;
+    public static final int REDIRECT_RESPONSE_CODE = 302;
+    public static final int INTERNAL_ERROR_RESPONSE_CODE = 500;
+    private static final List<String> goSupportedVersions = asList("1.0");
+    private static Logger LOGGER = Logger.getLoggerFor(StrongAuthPlugin.class);
+    private GoApplicationAccessor goApplicationAccessor;
 
     @Override
-    public GoPluginApiResponse handle(GoPluginApiRequest requestMessage) throws UnhandledRequestTypeException {
-        Logger.getLoggerFor(StrongAuthPlugin.class).warn(String.format("Received message: %s -- %s -- %s", requestMessage.requestParameters(), requestMessage.requestName(), requestMessage.requestBody()));
-
-        if (requestMessage.requestName().equals(PLUGIN_SETTINGS_GET_CONFIGURATION)) {
-            return handleGetPluginSettingsConfiguration();
-        }
-        return new DefaultGoPluginApiResponse(404);
+    public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
+        this.goApplicationAccessor = goApplicationAccessor;
     }
 
-    private GoPluginApiResponse handleGetPluginSettingsConfiguration() {
-        return new GoPluginApiResponse() {
-            @Override
-            public int responseCode() {
-                return 200;
-            }
-
-            @Override
-            public Map<String, String> responseHeaders() {
-                return new HashMap<>();
-            }
-
-            @Override
-            public String responseBody() {
-                Map<String, Object> configuration = new HashMap<>();
-                configuration.put("display-name", "Strong Auth Plugin");
-                configuration.put("display-image-url", "http://icons.iconarchive.com/icons/saki/snowish/32/Authentication-Lock-icon.png");
-                configuration.put("supports-web-based-authentication", true);
-                configuration.put("supports-password-based-authentication", true);
-                return JSONUtils.toJSON(configuration);
-            }
-        };
+    @Override
+    @SneakyThrows
+    public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) {
+        LOGGER.info(String.format("Received message: %s -- %s -- %s", goPluginApiRequest.requestParameters(), goPluginApiRequest.requestName(), goPluginApiRequest.requestBody()));
+        String requestName = goPluginApiRequest.requestName();
+        if (requestName.equals(PLUGIN_CONFIGURATION)) {
+            return handlePluginConfigurationRequest();
+        } else if (requestName.equals("go.plugin-settings.get-configuration")) {
+            return handlePluginConfigurationRequest();
+        } else if (requestName.equals("go.plugin-settings.get-view")) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("template", IOUtils.toString(getClass().getResourceAsStream("/plugin-settings.template.html"), "UTF-8"));
+            return renderResponse(SUCCESS_RESPONSE_CODE, null, JSONUtils.toJSON(response));
+        } else {
+            return renderResponse(404, null, null);
+        }
     }
 
     @Override
     public GoPluginIdentifier pluginIdentifier() {
-        return new GoPluginIdentifier(EXTENSION_NAME, SUPPORTED_VERSIONS);
+        return getGoPluginIdentifier();
+    }
+
+    private GoPluginApiResponse handlePluginConfigurationRequest() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("foo", createField("Server Base URL", null, true, false, "0"));
+        return renderResponse(SUCCESS_RESPONSE_CODE, null, JSONUtils.toJSON(response));
+    }
+
+    private Map<String, Object> createField(String displayName, String defaultValue, boolean isRequired, boolean isSecure, String displayOrder) {
+        Map<String, Object> fieldProperties = new HashMap<String, Object>();
+        fieldProperties.put("display-name", displayName);
+        fieldProperties.put("default-value", defaultValue);
+        fieldProperties.put("required", isRequired);
+        fieldProperties.put("secure", isSecure);
+        fieldProperties.put("display-order", displayOrder);
+        return fieldProperties;
+    }
+
+    private GoPluginIdentifier getGoPluginIdentifier() {
+        return new GoPluginIdentifier(EXTENSION_NAME, goSupportedVersions);
+    }
+
+    private GoPluginApiResponse renderResponse(final int responseCode, final Map<String, String> responseHeaders, final String responseBody) {
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return responseCode;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return responseHeaders;
+            }
+
+            @Override
+            public String responseBody() {
+                return responseBody;
+            }
+        };
     }
 }
