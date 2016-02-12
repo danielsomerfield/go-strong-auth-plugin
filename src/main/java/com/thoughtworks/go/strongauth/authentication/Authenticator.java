7f2d0130ca9bc0f098de7bba203f3fc9c5eb3f13
Daniel Somerfield <daniel@redwinewithfish.org>
2016-02-12 15:48:53 -0800

    First rough cut of authenticator is working.

--- src/main/java/com/thoughtworks/go/strongauth/authentication/Authenticator.java
+++ src/main/java/com/thoughtworks/go/strongauth/authentication/Authenticator.java
@@ -1,14 +1,60 @@
 package com.thoughtworks.go.strongauth.authentication;
 
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.thoughtworks.util.Functional;
+import lombok.SneakyThrows;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 public class Authenticator {
 
-    public Authenticator(PrincipalDetailSource principalDetailSource) {
+    private PrincipalDetailSource principalDetailSource;
 
+    public Authenticator(PrincipalDetailSource principalDetailSource) {
+        this.principalDetailSource = principalDetailSource;
     }
 
-    public Optional<Principal> authenticate(String username, String password) {
-        return Optional.absent();
+    public Optional<Principal> authenticate(final String username, final String password) {
+        return Functional.flatMap(principalDetailSource.byUsername(username), new Function<PrincipalDetail, Optional<Principal>>() {
+            @Override
+            public Optional<Principal> apply(PrincipalDetail principalDetail) {
+                return toPrincipal(principalDetail, password);
+            }
+        });
+    }
+
+    @SneakyThrows(UnsupportedEncodingException.class)
+    private static Optional<Principal> toPrincipal(PrincipalDetail principalDetail, String password) {
+
+        String hashConfig = principalDetail.getHashConfiguration();
+
+        PBEKeySpec spec = new PBEKeySpec(
+                password.toCharArray(),
+                principalDetail.getSalt().getBytes("UTF-8"),
+                10000,
+                256);
+
+        try {
+            byte[] key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1").generateSecret(spec).getEncoded();
+            return Arrays.equals(Hex.decodeHex(principalDetail.getPasswordHash().toCharArray()), key) ?
+                    Optional.of(new Principal(principalDetail.getUsername())) :
+                    Optional.<Principal>absent();
+
+        } catch (DecoderException e) {
+            //TODO: Log problem
+            return Optional.absent();
+        } catch (GeneralSecurityException e) {
+            //TODO: Log problem
+            return Optional.absent();
+        }
     }
 }
--- src/test/java/com/thoughtworks/go/strongauth/authentication/AuthenticatorTest.java
+++ src/test/java/com/thoughtworks/go/strongauth/authentication/AuthenticatorTest.java
@@ -15,14 +15,16 @@ public class AuthenticatorTest {
     private Authenticator authenticator = new Authenticator(principalDetailSource);
 
     @Test
-    @Ignore
     public void testAuthenticateUserSuccess() {
         String username = "username123";
         String password = "pazzword123";
 
         when(principalDetailSource.byUsername("username123")).thenReturn(
                 Optional.of(new PrincipalDetail(
-                        "username123", "salt", "21296e93b24064d688c934a6c22462cd5c74b927258c85e20eb1362921d15332", "PBKDF2WithHmacSHA1(10000, 256)"
+                        "username123",
+                        "21296e93b24064d688c934a6c22462cd5c74b927258c85e20eb1362921d15332",
+                        "salt",
+                        "PBKDF2WithHmacSHA1(10000, 256)"
                 )));
 
         Optional<Principal> maybePrincipal = authenticator.authenticate(username, password);
