34a118b2e3b7f3686ea233717d8f1d31b884c302
Daniel Somerfield <daniel@redwinewithfish.org>
2016-02-27 01:28:17 -0500

    Getting rid of a lot of old files from template.

--- src/main/java/com/thoughtworks/go/strongauth/config/StrongAuthPluginConfigurationProvider.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.go.strongauth.config;
-
-import com.google.common.base.Optional;
-
-public interface StrongAuthPluginConfigurationProvider {
-    Optional<String> getPasswordFileLocation();
-}
